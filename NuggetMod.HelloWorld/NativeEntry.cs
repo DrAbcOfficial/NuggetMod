566bfe9ab382adfa259c6b06a430816cf3d44282
Abc Dr <me@drabc.net>
2026-04-01 23:42:32 +0800

    example project

---
+++ NuggetMod.HelloWorld/NativeEntry.cs
@@ -0,0 +1,39 @@
+﻿using NuggetMod.Enum.Metamod;
+using NuggetMod.Interface;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace NuggetMod.HelloWorld;
+
+public class NativeEntry : PluginEntry
+{
+    static NativeEntry()
+    {
+        Interface = new Plugin();
+    }
+    [UnmanagedCallersOnly(EntryPoint = "GiveFnptrsToDll")]
+    public static void UnmanagedGiveFnptrsToDll(nint pengfuncsFromEngine, nint pGlobals)
+    {
+        Native_GiveFnptrsToDll(pengfuncsFromEngine, pGlobals);
+    }
+    [UnmanagedCallersOnly(EntryPoint = "Meta_Init", CallConvs = [typeof(CallConvCdecl)])]
+    public static void UnmanagedMeta_Init()
+    {
+        Native_Meta_Init();
+    }
+    [UnmanagedCallersOnly(EntryPoint = "Meta_Query", CallConvs = [typeof(CallConvCdecl)])]
+    public static int UnmanagedMeta_Query(nint interfaceVersion, nint plinfo, nint pMetaUtilFuncs)
+    {
+        return Native_Meta_Query(interfaceVersion, plinfo, pMetaUtilFuncs);
+    }
+    [UnmanagedCallersOnly(EntryPoint = "Meta_Attach", CallConvs = [typeof(CallConvCdecl)])]
+    public static int UnmanagedMeta_Attach(PluginLoadTime now, nint pFunctionTable, nint pMGlobals, nint pGamedllFuncs)
+    {
+        return Native_Meta_Attach(now, pFunctionTable, pMGlobals, pGamedllFuncs);
+    }
+    [UnmanagedCallersOnly(EntryPoint = "Meta_Detach", CallConvs = [typeof(CallConvCdecl)])]
+    public static int UnmanagedMeta_Detach(PluginLoadTime now, PluginUnloadReason reason)
+    {
+        return Native_Meta_Detach(now, reason);
+    }
+}
---
+++ NuggetMod.HelloWorld/Plugin.cs
@@ -0,0 +1,66 @@
+﻿using NuggetMod.Enum.Metamod;
+using NuggetMod.Helper;
+using NuggetMod.Interface;
+using NuggetMod.Interface.Events;
+using NuggetMod.Wrapper.Engine;
+using NuggetMod.Wrapper.Metamod;
+
+namespace NuggetMod.HelloWorld;
+
+/// <summary>
+/// Plugin entry point: the name must be Plugin and must inherit from the IPlugin interface.
+/// </summary>
+public class Plugin : IPlugin
+{
+    /// <summary>
+    /// Plugin information: it is recommended to set it as static to maintain memory availability.
+    /// </summary>
+    private readonly static MetaPluginInfo _pluginInfo = new()
+    {
+        InterfaceVersion = InterfaceVersion.V5_16,
+        Name = "HelloWorld",
+        Version = "1.0",
+        Author = "Dr.Abc",
+        Date = "2026/4/1",
+        LogTag = "HLWD",
+        Url = "github.com",
+        Loadable = PluginLoadTime.Anytime,
+        Unloadable = PluginLoadTime.Anytime
+    };
+
+    public MetaPluginInfo GetPluginInfo()
+    {
+        return _pluginInfo;
+    }
+
+    public void MetaInit()
+    {
+
+    }
+
+    public bool MetaQuery(InterfaceVersion interfaceVersion, MetaUtilFunctions pMetaUtilFuncs)
+    {
+        if (interfaceVersion != _pluginInfo.InterfaceVersion)
+            return false;
+        return true;
+    }
+
+    public bool MetaAttach(PluginLoadTime now, MetaGlobals pMGlobals, MetaGameDLLFunctions pGamedllFuncs)
+    {
+        CVar hello = new("hello", "World", CVar.FCVAR.Server);
+        hello.RegisterWith(MetaMod.EngineFuncs);
+        DLLEvents dLLEvents = new();
+        dLLEvents.ServerActivate += (pEdictList, edictCount, clientMax) =>
+        {
+            MetaMod.EngineFuncs.ServerPrint($"Hello, {hello.Str}!\n");
+            return MetaResult.Override;
+        };
+        MetaMod.RegisterEventsSafely(new EventRegistrationBuilder().WithEntityApi(dLLEvents));
+        return true;
+    }
+
+    public bool MetaDetach(PluginLoadTime now, PluginUnloadReason reason)
+    {
+        return true;
+    }
+}
